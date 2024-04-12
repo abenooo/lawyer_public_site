99246f7395e7f66bf8a7432ada3e426a7e31523e
Abenezer <abenezerkifle000@gmail.com>
2024-04-12 12:26:45 -0700

    Update button

--- pages/not-found.jsx
+++ pages/not-found.jsx
@@ -2,7 +2,6 @@
 
 import { useRouter } from "next/navigation";
 
-import { Button } from "@/components/ui/button";
 export default function NotFound() {
   const router = useRouter();
 
@@ -19,12 +18,12 @@ export default function NotFound() {
         moved.
       </p>
       <div className="mt-8 flex justify-center gap-2">
-        <Button onClick={() => router.back()} variant="default" size="lg">
+        <button onClick={() => router.back()} variant="default" size="lg">
           Go back
-        </Button>
-        <Button onClick={() => router.push("/")} variant="ghost" size="lg">
+        </button>
+        <button onClick={() => router.push("/")} variant="ghost" size="lg">
           Back to Home
-        </Button>
+        </button>
       </div>
       å
     </div>
