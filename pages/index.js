32c9e781910cedd183eb820ed18ba53bbcb6264e
Abenezer <abenezerkifle000@gmail.com>
2024-04-12 10:26:24 -0700

    Use navbar

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,12 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
-
+import Navbar from "@/components/navbar";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <main>
-      <h2>start</h2>
-    </main>
+    <>
+     <Navbar/>
+    </>
   );
 }
