f046785e87e689dd06fb4e8ccb73c623d9509df6
Abenezer <abenezerkifle000@gmail.com>
2024-04-13 07:04:51 -0700

    Clean navbar

--- components/navbar/index.jsx
+++ components/navbar/index.jsx
@@ -21,7 +21,7 @@ const MyNavbar = () => {
       <nav class="bg-white border-gray-200 dark:border-gray-600 dark:bg-gray-900">
         <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl p-4">
           <a
-            href="https://flowbite.com"
+            href="#"
             class="flex items-center space-x-3 rtl:space-x-reverse"
           >
             <img
