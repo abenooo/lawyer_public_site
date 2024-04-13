d151e6692f52f8607c6c9ea4e419cc34e7d4db3f
Abenezer <abenezerkifle000@gmail.com>
2024-04-13 05:04:25 -0700

    Display the Hero at home page

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,12 @@
 import "@/styles/globals.css";
 import Navbar from "@/components/navbar";
+import Hero from "@/components/navbar/Hero";
 export default function App({ Component, pageProps }) {
   return (
   <>
   <Navbar />
   <Component {...pageProps} />
+  <Hero/>
   </>
   )
 };
