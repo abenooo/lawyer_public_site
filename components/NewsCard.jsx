aaaf91664db3b9ec451b1493a0a388215f91d426
kasahun-welela <kasahunwelela1@gmail.com>
2024-04-15 21:03:58 +0300

    create news card component

---
+++ components/NewsCard.jsx
@@ -0,0 +1,13 @@
+
+function NewsCard() {
+  return (
+    <div className="mx-4">
+    <h1 className="text-4xl font-semibold text-center my-6">Latest News</h1>
+    <p className="text-center mb-5">
+      Keep up to date with our Australian law firm’s latest News.
+    </p>
+  </div>
+  )
+}
+
+export default NewsCard
