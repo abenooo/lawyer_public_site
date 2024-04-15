6e6be15fc3869ff2995351db76ec9a641426465c
kasahun-welela <kasahunwelela1@gmail.com>
2024-04-15 21:09:10 +0300

    add teams info using card

--- components/Teams.jsx
+++ components/Teams.jsx
@@ -11,6 +11,122 @@ function Teams() {
           their practice areas.
         </p>
       </div>
+      <div className="grid md:grid-cols-4 grid-cols-2 md:w-3/4 mx-3 md:mx-auto gap-5 -mt-24">
+        <div className="shadow-2xl text-center max-w-sm bg-white">
+          <img
+            src="https://slflawyers.com.au/wp-content/uploads/2022/06/Photo-30-6-2022-11-19-35-am.jpg"
+            className="hover:grayscale-[80%]"
+          />
+          <p className="text-center my-2 font-semibold">Rebecca Fahey</p>
+          <p className="text-center my-2">Managing Partner</p>
+          <div className="flex justify-center gap-5 my-3">
+            <BsLinkedin className="text-cyan-500 text-xl" />
+            <BsTelephoneFill className="text-cyan-500 text-xl" />
+            <BsEnvelope className="text-cyan-500 text-xl" />
+          </div>
+        </div>
+        <div className=" shadow-2xl text-center max-w-sm bg-white">
+          <img
+            src="https://slflawyers.com.au/wp-content/uploads/2022/06/SLF_Lawyers_Headshots_9919_Final_Digital.jpg"
+            className="hover:grayscale-[80%]"
+          />
+          <p className="text-center my-2 font-semibold">Mark Smith</p>
+          <p className="text-center my-2">Senior Partner</p>
+          <div className="flex justify-center gap-5 my-3">
+            <BsLinkedin className="text-cyan-500 text-xl" />
+            <BsTelephoneFill className="text-cyan-500 text-xl" />
+            <BsEnvelope className="text-cyan-500 text-xl" />
+          </div>
+        </div>
+        <div className=" shadow-2xl text-center max-w-sm bg-white">
+          <img
+            src="https://slflawyers.com.au/wp-content/uploads/2022/06/SLF_Lawyers_Headshots_11967_Final_Digital.jpg"
+            className="hover:grayscale-[80%]"
+          />
+          <p className="text-center my-2 font-semibold">John Gdanski</p>
+          <p className="text-center my-2">Senior Partner</p>
+          <div className="flex justify-center gap-5 my-3">
+            <BsLinkedin className="text-cyan-500 text-xl" />
+            <BsTelephoneFill className="text-cyan-500 text-xl" />
+            <BsEnvelope className="text-cyan-500 text-xl" />
+          </div>
+        </div>
+        <div className=" shadow-2xl text-center max-w-sm bg-white">
+          <img
+            src="https://slflawyers.com.au/wp-content/uploads/2022/07/Melissa-smallest.png"
+            className="hover:grayscale-[80%]"
+          />
+          <p className="text-center my-2 font-semibold">Melissa Patterson</p>
+          <p className="text-center my-2">Senior Partner</p>
+          <div className="flex justify-center gap-5 my-3">
+            <BsLinkedin className="text-cyan-500 text-xl" />
+            <BsTelephoneFill className="text-cyan-500 text-xl" />
+            <BsEnvelope className="text-cyan-500 text-xl" />
+          </div>
+        </div>
+        <div className=" shadow-2xl text-center max-w-sm bg-white">
+          <img
+            src="https://slflawyers.com.au/wp-content/uploads/2022/06/SLF_Lawyers_Headshots_14945_Final_Digital.jpg"
+            className="hover:grayscale-[80%]"
+          />
+          <p className="text-center my-2 font-semibold">Jason Quah</p>
+          <p className="text-center my-2">Partner</p>
+          <div className="flex justify-center gap-5 my-3">
+            <BsLinkedin className="text-cyan-500 text-xl" />
+            <BsTelephoneFill className="text-cyan-500 text-xl" />
+            <BsEnvelope className="text-cyan-500 text-xl" />
+          </div>
+        </div>
+        <div className=" shadow-2xl text-center max-w-sm bg-white">
+          <img
+            src="https://slflawyers.com.au/wp-content/uploads/2022/06/SLF_Lawyers_Headshots_14969_Final_Digital.jpg"
+            className="hover:grayscale-[80%]"
+          />
+          <p className="text-center my-2 font-semibold">Angela Smith</p>
+          <p className="text-center my-2">Partner</p>
+          <div className="flex justify-center gap-5 my-3">
+            <BsLinkedin className="text-cyan-500 text-xl" />
+            <BsTelephoneFill className="text-cyan-500 text-xl" />
+            <BsEnvelope className="text-cyan-500 text-xl" />
+          </div>
+        </div>
+
+        <div className=" shadow-2xl text-center max-w-sm bg-white">
+          <img
+            src=" https://slflawyers.com.au/wp-content/uploads/2022/06/SLF_Lawyers_Headshots_10893_Final_Digital-640x640.jpg"
+            className="hover:grayscale-[80%]"
+          />
+          <p className="text-center my-2 font-semibold">Charles Ampt</p>
+          <p className="text-center my-2">Partner</p>
+          <div className="flex justify-center gap-5 my-3">
+            <BsLinkedin className="text-cyan-500 text-xl" />
+            <BsTelephoneFill className="text-cyan-500 text-xl" />
+            <BsEnvelope className="text-cyan-500 text-xl" />
+          </div>
+        </div>
+
+        <div className=" shadow-2xl text-center max-w-sm bg-white">
+          <img
+            src="https://slflawyers.com.au/wp-content/uploads/2022/06/SLF_Lawyers_Headshots_12678_Final_Digital.jpg"
+            className="hover:grayscale-[80%]"
+          />
+          <p className="text-center my-2 font-semibold">Berndan Hoogenbosch</p>
+          <p className="text-center my-2">Partner</p>
+          <div className="flex justify-center gap-5 my-3">
+            <BsLinkedin className="text-cyan-500 text-xl" />
+            <BsTelephoneFill className="text-cyan-500 text-xl" />
+            <BsEnvelope className="text-cyan-500 text-xl" />
+          </div>
+        </div>
+      </div>
+      <div className="text-center">
+        <a
+          href="#"
+          class="inline-block mx-auto text-center mt-8 border border-cyan-600 px-8 py-3 font-medium hover:bg-black hover:text-white"
+        >
+          View More People
+        </a>
+      </div>
       
     </>
   )
