43791502e060f6f0bc09a5b01d1c0e3122fa150d
Abenezer <abenezerkifle000@gmail.com>
2024-04-15 07:53:01 -0700

    Fix the navbar items

--- components/navbar/index.jsx
+++ components/navbar/index.jsx
@@ -129,42 +129,112 @@ const MyNavbar = () => {
               id="mega-menu-full-dropdown"
               className="mt-1 border-gray-200 shadow-sm bg-gray-50 md:bg-white border-y dark:bg-gray-800 dark:border-gray-600"
             >
-              <div className="grid max-w-screen-xl px-4 py-5 mx-auto text-gray-900 dark:text-white sm:grid-cols-2 md:px-6">
+              <div className="grid max-w-screen-xl px-4 py-5 mx-auto text-gray-900 dark:text-white sm:grid-cols-3 md:px-6">
+              <ul>
+                  <li>
+                    <a href="/practice-area/insurance-law">
+                      <a className="block p-3 rounded-lg hover:bg-gray-100 dark:hover:bg-gray-700">
+                        <div className="font-semibold">Insurance Law</div>
+                        <span className="text-sm text-gray-500 dark:text-gray-400">
+                          Details about the insurance law practice.
+                        </span>
+                      </a>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="/practice-area/commercial-litigation/">
+                      <a className="block p-3 rounded-lg hover:bg-gray-100 dark:hover:bg-gray-700">
+                        <div className="font-semibold">
+                          Commercial Litigation
+                        </div>
+                        <span className="text-sm text-gray-500 dark:text-gray-400">
+                          Family law services and support.
+                        </span>
+                      </a>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="/practice-area/family-law">
+                      <a className="block p-3 rounded-lg hover:bg-gray-100 dark:hover:bg-gray-700">
+                        <div className="font-semibold">Family Law</div>
+                        <span className="text-sm text-gray-500 dark:text-gray-400">
+                          Family law services and support.
+                        </span>
+                      </a>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="/practice-area/commercial-law">
+                      <a className="block p-3 rounded-lg hover:bg-gray-100 dark:hover:bg-gray-700">
+                        <div className="font-semibold">Commercial Law</div>
+                        <span className="text-sm text-gray-500 dark:text-gray-400">
+                          Family law services and support.
+                        </span>
+                      </a>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="/practice-area/commercial-law">
+                      <a className="block p-3 rounded-lg hover:bg-gray-100 dark:hover:bg-gray-700">
+                        <div className="font-semibold">Commercial Law</div>
+                        <span className="text-sm text-gray-500 dark:text-gray-400">
+                          Family law services and support.
+                        </span>
+                      </a>
+                    </a>
+                  </li>
+                </ul>
                 <ul>
                   <li>
-                    <a
-                      href="#"
-                      className="block p-3 rounded-lg hover:bg-gray-100 dark:hover:bg-gray-700"
-                    >
-                      <div className="font-semibold">Insurance Law</div>
-                      <span className="text-sm text-gray-500 dark:text-gray-400">
-                        Connect with third-party tools that you're already
-                        using.
-                      </span>
+                    <a href="/practice-area/insurance-law">
+                      <a className="block p-3 rounded-lg hover:bg-gray-100 dark:hover:bg-gray-700">
+                        <div className="font-semibold">Insurance Law</div>
+                        <span className="text-sm text-gray-500 dark:text-gray-400">
+                          Details about the insurance law practice.
+                        </span>
+                      </a>
                     </a>
                   </li>
                   <li>
-                    <a
-                      href="#"
-                      className="block p-3 rounded-lg hover:bg-gray-100 dark:hover:bg-gray-700"
-                    >
-                      <div className="font-semibold">Insurance Law</div>
-                      <span className="text-sm text-gray-500 dark:text-gray-400">
-                        Connect with third-party tools that you're already
-                        using.
-                      </span>
+                    <a href="/practice-area/commercial-litigation/">
+                      <a className="block p-3 rounded-lg hover:bg-gray-100 dark:hover:bg-gray-700">
+                        <div className="font-semibold">
+                          Commercial Litigation
+                        </div>
+                        <span className="text-sm text-gray-500 dark:text-gray-400">
+                          Family law services and support.
+                        </span>
+                      </a>
                     </a>
                   </li>
                   <li>
-                    <a
-                      href="#"
-                      className="block p-3 rounded-lg hover:bg-gray-100 dark:hover:bg-gray-700"
-                    >
-                      <div className="font-semibold">Family Law</div>
-                      <span className="text-sm text-gray-500 dark:text-gray-400">
-                        Connect with third-party tools that you're already
-                        using.
-                      </span>
+                    <a href="/practice-area/family-law">
+                      <a className="block p-3 rounded-lg hover:bg-gray-100 dark:hover:bg-gray-700">
+                        <div className="font-semibold">Family Law</div>
+                        <span className="text-sm text-gray-500 dark:text-gray-400">
+                          Family law services and support.
+                        </span>
+                      </a>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="/practice-area/commercial-law">
+                      <a className="block p-3 rounded-lg hover:bg-gray-100 dark:hover:bg-gray-700">
+                        <div className="font-semibold">Commercial Law</div>
+                        <span className="text-sm text-gray-500 dark:text-gray-400">
+                          Family law services and support.
+                        </span>
+                      </a>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="/practice-area/commercial-law">
+                      <a className="block p-3 rounded-lg hover:bg-gray-100 dark:hover:bg-gray-700">
+                        <div className="font-semibold">Commercial Law</div>
+                        <span className="text-sm text-gray-500 dark:text-gray-400">
+                          Family law services and support.
+                        </span>
+                      </a>
                     </a>
                   </li>
                 </ul>
