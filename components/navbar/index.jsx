18ed35295da7ff8823c05aa773cbfe381163f47b
Abenezer <abenezerkifle000@gmail.com>
2024-04-15 07:56:50 -0700

    Fix the navbar items

--- components/navbar/index.jsx
+++ components/navbar/index.jsx
@@ -65,7 +65,7 @@ const MyNavbar = () => {
                   href="#"
                   className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-blue-500 md:dark:hover:bg-transparent dark:border-gray-700"
                 >
-                  News
+                  About
                 </a>
               </li>
               <li>
@@ -106,7 +106,7 @@ const MyNavbar = () => {
                   href="#"
                   className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-blue-500 md:dark:hover:bg-transparent dark:border-gray-700"
                 >
-                  News
+                  About Slf
                 </a>
               </li>
               <li>
