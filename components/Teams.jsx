80b7a69a9cd29dad37cee755fa23c7c9d28b509f
kasahun-welela <kasahunwelela1@gmail.com>
2024-04-15 21:07:54 +0300

    create teams component

---
+++ components/Teams.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Teams() {
+  return (
+    <>
+      <div className=" bg-[url('https://slflawyers.com.au/wp-content/uploads/2023/02/Untitled-design-2023-02-03T120754.566.png')] h-[540px] flex flex-col items-center justify-center text-white mt-8">
+        <h1 className="font-semibold text-center text-4xl ">Our Partners</h1>
+        <p className=" mt-16 font-medium mx-3 text-center">
+          Our team of the best lawyers in Australia have combined experience of
+          over 750 years and have decades of hands-on experience in each of
+          their practice areas.
+        </p>
+      </div>
+      
+    </>
+  )
+}
+
+export default Teams
