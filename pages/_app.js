cca81949622463afcb9bfa10358855b524e0663e
Abenezer <abenezerkifle000@gmail.com>
2024-04-15 07:52:28 -0700

    Display the Hero section only at home page

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,17 @@
 import "@/styles/globals.css";
 import MyNavbar from "@/components/navbar";
 import Hero from "@/components/Hero";
+import { useRouter } from "next/router";
+
 export default function App({ Component, pageProps }) {
+  const router = useRouter();
+  const isHomePage = router.pathname === "/";
+
   return (
-  <>
-  <MyNavbar />
-  <Component {...pageProps} />
-  <Hero/>
-  </>
-  )
-};
+    <>
+      <MyNavbar />
+      {isHomePage && <Hero />}
+      <Component {...pageProps} />
+    </>
+  );
+}
