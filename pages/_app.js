0ccab93f9b7ab649173611c6a7995dfa09693120
Abenezer Kifle <52881874+abenooo@users.noreply.github.com>
2024-04-17 17:32:07 +0300

    Update _app.js remove contact us from all pages

--- pages/_app.js
+++ pages/_app.js
@@ -13,7 +13,6 @@ export default function App({ Component, pageProps }) {
       <MyNavbar />
       {isHomePage && <Hero />}
       <Component {...pageProps} />
-      <ContactUs/>
     </>
   );
 }
