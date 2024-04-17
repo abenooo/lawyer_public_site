66014539edc9b12c20a3cea7e2fb28d6359d0d70
kasahun welela <getasewmequanint@cbe.com.et>
2024-04-17 17:22:27 +0300

    add hover effect and change text color

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -9,7 +9,7 @@ import { FaLinkedinIn } from "react-icons/fa6";
 
 function Footer() {
   return (
-    <div className="bg-slate-900 text-gray-200 py-20 px-8 mt-10">
+    <div className="bg-slate-900 text-gray-400 py-20 px-8 mt-10">
       <div className="grid grid-cols-2 md:grid-cols-4 gap-8 md:w-3/4 mx-auto">
         <div className="col-span-2 md:col-span-1">
           <div className="flex gap-3">
@@ -18,7 +18,9 @@ function Footer() {
               alt="logo"
               className="h-8 w-8 rounded-xl"
             />
-            <h3 className="text-lg font-semibold">Solomon lawyer</h3>
+            <h3 className="text-lg font-semibold text-gray-200">
+              Solomon lawyer
+            </h3>
           </div>
 
           <p className="text-justify mt-2">
@@ -28,48 +30,68 @@ function Footer() {
           </p>
         </div>
         <div className="mt-3 md:ml-9">
-          <h3 className="text-lg font-semibold">Quick Links</h3>
+          <h3 className="text-lg font-semibold text-gray-200">Quick Links</h3>
           <ul className="ml-3">
             <li>
-              <a href="#">Home</a>
+              <a href="#" className="hover:underline hover:text-gray-200">
+                Home
+              </a>
             </li>
             <li>
-              <a href="#">About Us</a>
+              <a href="#" className="hover:underline hover:text-gray-200">
+                About Us
+              </a>
             </li>
             <li>
-              <a href="#">Contact Us</a>
+              <a href="#" className="hover:underline hover:text-gray-200">
+                Contact Us
+              </a>
             </li>
             <li>
-              <a href="#">News</a>
+              <a href="#" className="hover:underline hover:text-gray-200">
+                News
+              </a>
             </li>
             <li>
-              <a href="#">Service</a>
+              <a href="#" className="hover:underline hover:text-gray-200">
+                Service
+              </a>
             </li>
           </ul>
         </div>
         <div className="mt-3">
-          <h3 className="text-lg font-semibold">Practice Area</h3>
+          <h3 className="text-lg font-semibold text-gray-200">Practice Area</h3>
           <ul className="ml-3">
             <li>
-              <a href="#">Family Law</a>
+              <a href="#" className="hover:underline hover:text-gray-200">
+                Family Law
+              </a>
             </li>
             <li>
-              <a href="#">Commercial Law</a>
+              <a href="#" className="hover:underline hover:text-gray-200">
+                Commercial Law
+              </a>
             </li>
             <li>
-              <a href="#">Insurance Law</a>
+              <a href="#" className="hover:underline hover:text-gray-200">
+                Insurance Law
+              </a>
             </li>
             <li>
-              <a href="#">Commercial Litigation</a>
+              <a href="#" className="hover:underline hover:text-gray-200">
+                Commercial Litigation
+              </a>
             </li>
             <li>
-              <a href="#">Service</a>
+              <a href="#" className="hover:underline hover:text-gray-200">
+                Service
+              </a>
             </li>
           </ul>
         </div>
 
         <div className="mt-3 col-span-2 md:col-span-1">
-          <h3 className="text-lg font-semibold">Information</h3>
+          <h3 className="text-lg font-semibold text-gray-200">Information</h3>
           <div className="flex items-center gap-3 my-2 ml-3">
             <GrLocation className="text-xl text-cyan-600" />
             <p className="text-sm">Addis Ababa lideta infront of xxx</p>
@@ -85,23 +107,38 @@ function Footer() {
         </div>
       </div>
       <div className="flex justify-center items-center gap-2 my-8">
-        <a href="#" className="bg-gray-700 p-3 rounded-full text-white">
+        <a
+          href="#"
+          className="bg-gray-700 hover:bg-gray-400 p-3 rounded-full text-white"
+        >
           {" "}
           <FaFacebookF />
         </a>
-        <a href="#" className="bg-gray-700 p-3 rounded-full text-white">
+        <a
+          href="#"
+          className="bg-gray-700 hover:bg-gray-400 p-3 rounded-full text-white"
+        >
           {" "}
           <FaXTwitter />
         </a>
-        <a href="#" className="bg-gray-700 p-3 rounded-full text-white">
+        <a
+          href="#"
+          className="bg-gray-700 hover:bg-gray-400 p-3 rounded-full text-white"
+        >
           {" "}
           <GrInstagram />
         </a>
-        <a href="#" className="bg-gray-700 p-3 rounded-full text-white">
+        <a
+          href="#"
+          className="bg-gray-700 hover:bg-gray-400 p-3 rounded-full text-white"
+        >
           {" "}
           <FaTelegramPlane />
         </a>
-        <a href="#" className="bg-gray-700 p-3 rounded-full text-white">
+        <a
+          href="#"
+          className="bg-gray-700 hover:bg-gray-400 p-3 rounded-full text-white"
+        >
           <FaLinkedinIn />
         </a>
       </div>
