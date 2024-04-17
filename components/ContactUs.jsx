619354e30217693f3269247089f513301329e9a0
kasahun welela <getasewmequanint@cbe.com.et>
2024-04-17 10:48:52 +0300

    add intro to component

--- components/ContactUs.jsx
+++ components/ContactUs.jsx
@@ -1,5 +1,22 @@
 function ContactUs() {
-  return <div>ContactUs</div>;
+  return (
+    <div>
+      <h1 className="font-semibold text-center text-4xl mt-14 ">
+        Do you need legal advice?{" "}
+      </h1>
+      <div class="flex justify-center">
+        <hr className="w-24 h-0.5 mt-5 mb-8 bg-yellow-600 border-0 rounded" />
+      </div>{" "}
+      <p className="my-3 text-center">
+        We understand that you may need help, but aren’t yet ready to enter into
+        a retainer
+      </p>
+      <p className="my-3 text-center">
+        If you need a confidential obligation-free consultation, contact us
+        today.
+      </p>
+    </div>
+  );
 }
 
 export default ContactUs;
