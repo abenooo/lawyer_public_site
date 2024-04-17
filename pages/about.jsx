9b25b2c233fbd88a019170405f244bbbb27c0515
Abenezer <abenezerkifle000@gmail.com>
2024-04-17 10:11:22 -0700

    Fix font for medium and large media

--- pages/about.jsx
+++ pages/about.jsx
@@ -71,13 +71,13 @@ const About = () => {
         <div className="w-full md:w-2/5">
           <div className=" bg-white shadow-md">
             <div className="bg-white rounded-t-3xl items-center gap-6  md:flex-row justify-around md:p-10">
-              <p className="text-left font-sans  text-4xl py-5">What we do?</p>
+              <p className="font-sans  text-4xl py-5 md:text-5xl lg:text-6xl text-center">What we do?</p>
 
-              <p className="font-sans text-xs text-left  text-gray-900">
+              <p className="font-sans text-xs text-left px-4  text-gray-900">
                 SLF Lawyers is a new breed of law firm. We don't stand on
                 tradition or outdated ways of doing business.
               </p>
-              <p className="font-sans text-xs text-left  text-gray-900 my-4">
+              <p className="font-sans text-xs text-left px-4  text-gray-900 my-4">
                 We take a team approach to dealing with the more complex legal
                 problems that cross our desks, working on the very reasonable
                 assumption that two or more heads are better than one. We take
@@ -85,7 +85,7 @@ const About = () => {
                 outside the box to find solutions in even the most complex legal
                 situations.
               </p>
-              <p className="font-sans text-xs text-left text-gray-900 my-4">
+              <p className="font-sans text-xs text-left px-4 text-gray-900 my-4">
                 We don't hide behind legalese or arcane legal procedures,
                 clearly communicating our process every step of the way. The
                 focus is on getting results for our clients, not on how our
