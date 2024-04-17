8faafa22dcbe97d98de4287431141fed161cfd3e
Abenezer <abenezerkifle000@gmail.com>
2024-04-17 00:24:36 -0700

    Fix the font style and size

--- pages/about.jsx
+++ pages/about.jsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import React from "react";
 
 const About = () => {
@@ -38,20 +39,20 @@ const About = () => {
     <>
       <div className="bg-[url('https://slflawyers.com.au/wp-content/uploads/2023/02/Untitled-design-2023-02-03T120344.159.png')] h-[50vh] bg-cover">
         <div className="text-white flex flex-col justify-center items-center">
-          <h1 className="lg:mt-40 md:mt-20 text-white font-semibold text-6xl">
+          <h1 className=" font-sans lg:mt-40 md:mt-20 text-white font-semibold text-6xl">
             About SLF Lawyers
           </h1>
         </div>
       </div>
       <div className="-mt-20 shadow-2xl w-11/12 mx-auto rounded-3xl md:w-3/5">
         <div className="bg-white rounded-t-3xl items-center gap-6 py-3 md:flex-row justify-around md:p-10">
-          <p>
+          <p className="font-sans text-sm">
             {" "}
             SLF Lawyers is a new breed of law firm. We don’t stand on tradition
             or outdated ways of doing business.
           </p>
 
-          <p>
+          <p className="font-sans text-sm">
             We take a team approach to dealing with the more complex legal
             problems that cross our desks, working on the very reasonable
             assumption that two or more heads are better than one. We take the
@@ -59,7 +60,7 @@ const About = () => {
             the box to find solutions in even the most complex legal situations.
           </p>
 
-          <p>
+          <p className="font-sans text-sm">
             We don’t hide behind legalese or arcane legal procedures, clearly
             communicating our process every step of the way. The focus is on
             getting results for our clients, not on how our foyer looks.
@@ -67,15 +68,16 @@ const About = () => {
         </div>
       </div>
       <div className="flex flex-wrap md:w-[80%] mx-auto my-10">
-        <div className="w-full md:w-2/5 my-20">
+        <div className="w-full md:w-2/5">
           <div className=" bg-white shadow-md">
             <div className="bg-white rounded-t-3xl items-center gap-6  md:flex-row justify-around md:p-10">
-              <p className="text-left  text-4xl py-5">What we do?</p>
-              <p className="text-left  text-gray-900 dark:text-gray-800">
+              <p className="text-left font-sans  text-4xl py-5">What we do?</p>
+
+              <p className="font-sans text-xs text-left  text-gray-900 dark:text-gray-800">
                 SLF Lawyers is a new breed of law firm. We don't stand on
                 tradition or outdated ways of doing business.
               </p>
-              <p className="text-left  text-gray-900 dark:text-gray-800 my-4">
+              <p className="font-sans text-xs text-left  text-gray-900 dark:text-gray-800 my-4">
                 We take a team approach to dealing with the more complex legal
                 problems that cross our desks, working on the very reasonable
                 assumption that two or more heads are better than one. We take
@@ -83,7 +85,7 @@ const About = () => {
                 outside the box to find solutions in even the most complex legal
                 situations.
               </p>
-              <p className="text-left text-gray-900 dark:text-gray-800 my-4">
+              <p className="font-sans text-xs text-left text-gray-900 dark:text-gray-800 my-4">
                 We don't hide behind legalese or arcane legal procedures,
                 clearly communicating our process every step of the way. The
                 focus is on getting results for our clients, not on how our
@@ -103,16 +105,16 @@ const About = () => {
                 >
                   <div
                     className="relative mb-9 rounded-xl py-8 px-7 bg-white shadow-md transition-all hover:shadow-lg sm:p-9 lg:px-6 xl:px-9"
-                    style={{ height: "200px", width: "100%" }}
+                    style={{ height: "100px", width: "100%" }}
                   >
                     <div className="absolute top-10 left-1/2 transform -translate-x-1/2 -translate-y-full w-24 h-24 bg-white border-4 border-white rounded-full overflow-hidden">
                       <img
-                        className="w-full h-full object-cover"
+                        className="w-16 h-16 object-cover"
                         src={category.image}
                         alt={category.name}
                       />
                     </div>
-                    <h3 className="pt-16 text-base font-medium text-body-color text-center">
+                    <h3 className="pt-2 text-base font-medium text-body-color text-center ">
                       {category.name}
                     </h3>
                   </div>
