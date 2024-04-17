6ec8155d34b72894b68a1cbfb425b82314e49771
kasahun welela <getasewmequanint@cbe.com.et>
2024-04-17 10:53:14 +0300

    update font style  and margin

--- components/ContactUs.jsx
+++ components/ContactUs.jsx
@@ -1,17 +1,17 @@
 function ContactUs() {
   return (
-    <div>
+    <div className="mx-6">
       <h1 className="font-semibold text-center text-4xl mt-14 ">
         Do you need legal advice?{" "}
       </h1>
       <div class="flex justify-center">
         <hr className="w-24 h-0.5 mt-5 mb-8 bg-yellow-600 border-0 rounded" />
       </div>{" "}
-      <p className="my-3 text-center">
+      <p className="my-3 text-center font-sans text-xs">
         We understand that you may need help, but aren’t yet ready to enter into
         a retainer
       </p>
-      <p className="my-3 text-center">
+      <p className="my-3 text-center font-sans text-xs">
         If you need a confidential obligation-free consultation, contact us
         today.
       </p>
