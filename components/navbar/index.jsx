f0281a489f5d9364358967b3477cbb3d2c5be3d2
kasahun welela <getasewmequanint@cbe.com.et>
2024-04-18 10:52:08 +0300

    capitalize

--- components/navbar/index.jsx
+++ components/navbar/index.jsx
@@ -114,7 +114,7 @@ const MyNavbar = () => {
                   href="contactUs"
                   className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-blue-500 md:dark:hover:bg-transparent dark:border-gray-700"
                 >
-                  Contact us
+                  Contact Us
                 </a>
               </li>
             </ul>
