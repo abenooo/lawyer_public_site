2cee963a439ee99c5dbe9ededc17d733ce9aee7f
kasahun welela <getasewmequanint@cbe.com.et>
2024-04-18 10:01:04 +0300

    add url

--- components/navbar/index.jsx
+++ components/navbar/index.jsx
@@ -111,7 +111,7 @@ const MyNavbar = () => {
               </li>
               <li>
                 <a
-                  href="#"
+                  href="contactUs"
                   className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-blue-500 md:dark:hover:bg-transparent dark:border-gray-700"
                 >
                   Contact us
@@ -130,7 +130,7 @@ const MyNavbar = () => {
               className="mt-1 border-gray-200 shadow-sm bg-gray-50 md:bg-white border-y dark:bg-gray-800 dark:border-gray-600"
             >
               <div className="grid max-w-screen-xl px-4 py-5 mx-auto text-gray-900 dark:text-white sm:grid-cols-3 md:px-6">
-              <ul>
+                <ul>
                   <li>
                     <a href="/practice-area/insurance-law">
                       <a className="block p-3 rounded-lg hover:bg-gray-100 dark:hover:bg-gray-700">
