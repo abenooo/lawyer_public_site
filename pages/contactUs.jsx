99de125e7ff9aa9f645d55779855d3e1914e76e6
kasahun welela <getasewmequanint@cbe.com.et>
2024-04-18 10:02:16 +0300

    create contact us page

---
+++ pages/contactUs.jsx
@@ -0,0 +1,17 @@
+
+
+const contactUs = () => {
+  return (
+    <>
+      <div className="bg-[url('https://slflawyers.com.au/wp-content/uploads/2023/02/Untitled-design-2023-02-03T120344.159.png')] h-[50vh] bg-cover">
+        <div className="text-white flex  justify-center items-center h-full text-center">
+          <h1 className=" font-sans text-white font-semibold text-4xl md:6xl lg:6xl">
+            Contact US
+          </h1>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default contactUs;
