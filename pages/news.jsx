181af6cb8fe0d27ae6202c5e76f4ac7280b270ed
Abenezer <abenezerkifle000@gmail.com>
2024-04-18 01:41:35 -0700

    Add news category daynammically

--- pages/news.jsx
+++ pages/news.jsx
@@ -1,12 +1,27 @@
+import React, { useEffect, useState } from 'react';
 import NewsCard from '@/components/NewsCard'
-import React from 'react'
+function news() {
+  const [categories, setCategories] = useState([]);
 
-const news = () => {
+  useEffect(() => {
+    fetch('https://lawyerpw.onrender.com/api/newsCategory')
+      .then(response => response.json())
+      .then(data => setCategories(data))
+      .catch(error => console.error('Error fetching data: ', error));
+  }, []);
+
   return (
-   <>
-   <NewsCard />
-   </>
-  )
+    <>
+    <div className="bg-white rounded-t-3xl py-3 md:flex-row justify-around md:p-10 flex space-x-2">
+      {categories.map(category => (
+        <button key={category._id} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+          {category.NewsCategoryName}
+        </button>
+      ))}
+    </div>
+    
+    <NewsCard /></>
+  );
 }
 
-export default news
+export default news;
