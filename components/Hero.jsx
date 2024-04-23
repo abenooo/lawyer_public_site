3db587121fbb47cdd7e7c9780664d41cd8f73adb
Abenezer <abenezerkifle000@gmail.com>
2024-04-23 07:04:24 -0700

    Change the image url

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,36 +1,38 @@
 import Teams from "./Teams";
 import NewsCard from "./NewsCard"
 import PracticeCard from "./PracticeCard"
+import Image from "next/image";
 function Hero() {
   return (
     <>
-      <div className=" bg-[url('https://slflawyers.com.au/wp-content/uploads/2023/02/Untitled-design-2023-02-03T120344.159.png')] h-[85vh] bg-cover">
-        <div className=" text-white flex flex-col gap-5 justify-center items-center h-full">
-          <h1 className=" text-white font-semibold text-4xl md:text-5xl lg:6px xl:7xl">
-            SLF Lawyers
-          </h1>
-          <p className="mx-3 text-center text-medium md:w-2/5">
-            SLF Lawyers is a new breed of law firm. We don’t stand on tradition
-            or outdated ways of doing business. We don’t hide behind legalese or
-            arcane legal procedures. The focus is on getting results for our
-            clients, not on how our foyer looks.
-          </p>
-          <div className="md:flex pb-28 pt-10 space-x-3">
-            <a
-              className="border border-cyan-600 px-8 py-3 font-medium hover:bg-white hover:text-black"
-              href="#"
-            >
-              Contact us
-            </a>
-            <a
-              className="border border-cyan-600 px-8 py-3 font-medium hover:bg-white hover:text-black"
-              href="#"
-            >
-              Practice area
-            </a>
-          </div>
-        </div>
-      </div>
+     <div className="bg-[url('/assets/homepage.png')] h-[85vh] bg-cover">
+  <div className="text-white flex flex-col gap-5 justify-center items-center h-full">
+    <h1 className="text-white font-semibold text-4xl md:text-5xl lg:6xl xl:7xl">
+      SLF Lawyers
+    </h1>
+    <p className="mx-3 text-center text-medium md:w-2/5">
+      SLF Lawyers is a new breed of law firm. We don’t stand on tradition
+      or outdated ways of doing business. We don’t hide behind legalese or
+      arcane legal procedures. The focus is on getting results for our
+      clients, not on how our foyer looks.
+    </p>
+    <div className="md:flex pb-28 pt-10 space-x-3">
+      <a
+        className="border border-cyan-600 px-8 py-3 font-medium hover:bg-white hover:text-black"
+        href="#"
+      >
+        Contact us
+      </a>
+      <a
+        className="border border-cyan-600 px-8 py-3 font-medium hover:bg-white hover:text-black"
+        href="#"
+      >
+        Practice area
+      </a>
+    </div>
+  </div>
+</div>
+
       {/* <div className=" z-50 border border-red-600"> */}
       <div className="-mt-20 shadow-2xl w-11/12 mx-auto  rounded-3xl  md:w-3/5 ">
         <div className=" bg-gray-100 rounded-t-3xl flex flex-col items-center gap-6 py-3 font-medium md:flex-row justify-around md:p-10">
