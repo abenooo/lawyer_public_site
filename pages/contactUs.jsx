141ac80c57de7c57568449465525152ef04fc3d3
Abenezer <abenezerkifle000@gmail.com>
2024-04-23 04:45:30 -0700

    Update the text

--- pages/contactUs.jsx
+++ pages/contactUs.jsx
@@ -10,6 +10,24 @@ const contactUs = () => {
           </h1>
         </div>
       </div>
+      <div className="text-center my-5">
+        <div className="-mt-20 shadow-2xl w-11/12 mx-auto rounded-3xl md:w-3/5">
+          <div className="bg-white rounded-t-3xl py-3 md:flex-row justify-around md:p-10">
+          <div class="w-full bg-white h-full lg:mx-10 px-5">
+        <h1 className="mt-10 md:m-0 text-center font-semibold text-xl">
+          Get In Touch
+        </h1>
+        <p className="text-justify mt-3">
+          Our team of experienced Lawyers is here to provide you with expert
+          guidance and support. Whether you have questions about a specific
+          case, need advice on legal matters, or want to schedule a
+          consultation, we're here to help. Let's work together to find the best
+          solution for your legal needs!
+        </p>
+        </div>
+          </div>
+        </div>
+      </div>
       <ContactUs />
     </>
   );
