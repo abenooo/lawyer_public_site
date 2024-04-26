94643f1e933a2198e597146138b673167f10c154
Abenezer <abenezerkifle000@gmail.com>
2024-04-26 08:11:37 -0700

    Add custome style for the map

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,9 @@
+import "../styles/leaflet.css";
 import "@/styles/globals.css";
 import MyNavbar from "@/components/navbar";
 import Hero from "@/components/Hero";
 import { useRouter } from "next/router";
 import Footer from "@/components/Footer";
-
 
 export default function App({ Component, pageProps }) {
   const router = useRouter();
