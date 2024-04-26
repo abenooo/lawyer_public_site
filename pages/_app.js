0f30a051657687e1c0af44cb880aa9d7bf22428e
Abenezer <abenezerkifle000@gmail.com>
2024-04-26 09:00:28 -0700

    Add what's up in everypage

--- pages/_app.js
+++ pages/_app.js
@@ -4,15 +4,21 @@ import MyNavbar from "@/components/navbar";
 import Hero from "@/components/Hero";
 import { useRouter } from "next/router";
 import Footer from "@/components/Footer";
-
+import { IoLogoWhatsapp } from "react-icons/io5";
 export default function App({ Component, pageProps }) {
   const router = useRouter();
   const isHomePage = router.pathname === "/";
-
+  const phoneNumber = "+251910979060"; // Replace 'YourPhoneNumber' with your actual number
+  const message = encodeURIComponent("Hello, I'm interested in your services!"); 
   return (
     <>
       <MyNavbar />
       {isHomePage && <Hero />}
+      <div className="fixed bottom-0 right-0 mb-4 mr-4">
+            <a href={`https://wa.me/${phoneNumber}?text=${message}`} target="_blank" rel="noopener noreferrer">
+                <IoLogoWhatsapp className="h-20 w-20 bg-green-600 rounded-full shadow-lg hover:bg-green-700" />
+            </a>
+        </div>
       <Component {...pageProps} />
       <Footer />
     </>
