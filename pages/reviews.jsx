d6a0c78a6f36d772314dfbf30cdf50c6f3273d91
Abenezer <abenezerkifle000@gmail.com>
2024-04-29 00:48:36 -0700

    Create reviews page

---
+++ pages/reviews.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Reviews = () => {
+
+  return (
+    <div>
+      <div className="container mx-auto px-4 py-8">
+        <div className="flex flex-wrap ">
+          <div className="w-full md:w-1/2 px-6">
+          <div className="w-full md:w-1/2 mt-10">
+          </div>hello theis is reveiw
+        </div>
+      </div>
+    </div>
+    </div>
+  );
+};
+
+export default Reviews;
