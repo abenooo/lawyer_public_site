34f054425db5547d3f538c4602ff3c15ca9e69d0
Abenezer <abenezerkifle000@gmail.com>
2024-06-08 07:40:12 -0700

    Add Disclaimer componet

---
+++ components/Disclaimer.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const Disclaimer = () => {
+  return (
+    <div className="disclaimer-container p-8 bg-white rounded-lg shadow-lg mx-auto mt-8 max-w-4xl">
+      <h1 className="text-3xl font-bold mb-6">DISCLAIMER BY Solomon Mulugeta  & ASSOCIATES</h1>
+      <h4 className="text-3xl font-bold mb-6">No Legal Advice Intended</h4>
+      <p className="mb-4">
+      The contents in the practice areas and in the website in general are intended to convey general information only and not to provide legal advice. The contents of this website, and the posting and viewing of the information on this website, should not be construed as, and should not be relied upon for, legal, investment, or other advice in any particular circumstance or fact situation.
+      </p>
+      <p className="mb-4">
+      The information provided on our website may not reflect the most current legal developments in the Federal Democratic Republic of Ethiopia.  We recommend that you take no legal action in reliance on the information contained on this website – you should always consult legal advice for specific issues.
+      </p>
+      <h2 className="text-2xl font-semibold mb-4">No Lawyer - Client Relationship Created</h2>
+      <p className="mb-4">
+      Nothing on this website is an offer to represent you, and nothing on this website is intended to create lawyer- client relationship. Any communication, including unsolicited email sent to Solomon Mulugeta & Associates Law Office, or to any of its staff members available on this website, will not create a lawyer-client relationship and the contents of such unsolicited email shall not be considered confidential.  Therefore, do not use this website or the email addresses available on this website to provide confidential information about yourself.
+      </p>
+      <h2 className="text-2xl font-semibold mb-4">Authorized Activity</h2>
+      <p className="mb-4">
+      This website is not intended as advertising or as solicitation for legal services.
+      </p>
+    </div>
+  );
+};
+
+export default Disclaimer;
