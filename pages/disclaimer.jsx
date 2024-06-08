cfff7fa5ffea74c4266619cc2e678f6c2a8ad840
Abenezer <abenezerkifle000@gmail.com>
2024-06-08 07:39:54 -0700

    Add disclaimer page

---
+++ pages/disclaimer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Disclaimer from '@/components/Disclaimer'
+const disclaimer = () => {
+  return (
+    <div>
+        <Disclaimer />
+        </div>
+  )
+}
+
+export default disclaimer
