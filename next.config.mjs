d7b7480b2f7a21cc5a5da7d0a89bd36962fb1d50
Abenezer <abenezerkifle000@gmail.com>
2024-06-26 23:36:24 -0700

    fix build error

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  trailingSlash: true, // Ensure trailing slashes for all routes
-  async rewrites() {
-    return [
-      {
-        source: '/news/:id',
-        destination: '/news/[id]',
-      },
-    ];
-  },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
