ee90d6073d184a0a46a59b3f70c33fa9f464b47d
Abenezer <abenezerkifle000@gmail.com>
2024-06-26 23:04:28 -0700

    update the config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  trailingSlash: true, // Ensure trailing slashes for all routes
+  async rewrites() {
+    return [
+      {
+        source: '/news/:id',
+        destination: '/news/[id]',
+      },
+    ];
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
